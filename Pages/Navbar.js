5b41a3365aaec5719de179f1dd9bcaa0b1e263af
Pursottam6003 <rahulsah6003@gmail.com>
2023-01-03 16:42:18 +0530

    added the demo barcode scanner

---
+++ Pages/Navbar.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { Image } from 'react-native'
+
+const Navbar = () => (
+   <Image source = {require('C:\Desktop Files\appdevelopment\smart-naka\assets\Images\Assam_Police.png')} />
+)
+export default Navbar   
